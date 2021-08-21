6273d21d91a088a51f77b73bc6e9266cced76080
s15p004 <askih.9764.hj@gmail.com>
2021-08-21 14:24:01 +0900

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,26 @@
+/* Projects		: 															*/
+/* Features		: 															*/
+/*  																		*/
+/* Author		: Hiroki Ishikawa											*/
+/* Corrector	: 															*/
+/* Date			: YYYY/MM/DD												*/
+#include <stdio.h>
+/* Function		: main														*/
+/* Features		: 															*/
+/* Author		: Hiroki Ishikawa											*/
+/* Corrector	: 															*/
+/* Date			: YYYY/MM/DD												*/
+int main(void) {
+	puts("Hello !! \n");
+	return 0;
+}
+
+/* Function		: 															*/
+/* Features		: 															*/
+/* Arguments	: 	 														*/
+/* 					 														*/
+/* Return value	:	 														*/
+/*  																		*/
+/* Author		: Hiroki Ishikawa											*/
+/* Corrector	: 															*/
+/* Date			: YYYY/MM/DD												*/
